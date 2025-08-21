83df96a908ea3a772bb5421b23f8b2f049b05d21
Osariemen7@gmail.com <osariemwen7@gmail.com>
2025-08-21 09:34:45 +0100

    updated viteconfig  file

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,3 @@
-import { createHash } from 'crypto';
-
-globalThis.crypto = {
-    ...globalThis.crypto,
-    subtle: globalThis.crypto?.subtle,
-    getRandomValues: globalThis.crypto?.getRandomValues,
-    hash: (alg) => createHash(alg),
-};
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
